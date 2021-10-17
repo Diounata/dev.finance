7aceffbf6c24913a0565ed4e48332dbb94c618c8
Diounata <jonathamcordeiro42@gmail.com>
2021-10-17 00:13:18 -0300

    Add: PWA support

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,10 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap" rel="stylesheet" />
+                   
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/favicon.svg" />
+                    <meta name="theme-color" content="#f0f1f5" />
                 </Head>
                 
                 <body>
